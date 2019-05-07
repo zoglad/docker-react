69c0d07b190f12148d8a0522a05b5f9c8859e64c
eladgu <eladgu@cloudzone.io>
2019-05-07 11:50:04 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            I was changed AGAIN!
+            I was changed on the Feature branch
           </p>
           <a
             className="App-link"
